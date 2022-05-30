4c18e3093033ff20f69328611a0cf42ea94163e3
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-05-30 14:30:49 +0200

    Cart adjust

--- src/components/ShowProducts.tsx
+++ src/components/ShowProducts.tsx
@@ -165,9 +165,9 @@ export const ShowProducts = () => {
         <Search />
         <Cart cart={cart} openCart={openCart} />
       </nav>
+      {cartItems}
       <Header />
       <CategoriesBtns />
-      {cartItems}
       {modalHtml}
       <div className="movie-main-container">{showMovies}</div>
       {paymentHtml}
