0d3634a6694b0df939fbb2d40c449b294f3cd3bd
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-05-30 14:18:29 +0200

    Categories btns

--- src/components/CategoriesBtns.tsx
+++ src/components/CategoriesBtns.tsx
@@ -1,13 +1,15 @@
 export const CategoriesBtns = () => {
   return (
     <>
-      {/* <div className="categories-btns-container">
-        <button className="categories-btn">All movies</button>
-        <button className="categories-btn">Action</button>
-        <button className="categories-btn">Comedy</button>
-        <button className="categories-btn">Sci Fi</button>
-        <button className="categories-btn">Thriller</button>
-      </div> */}
+      <div className="categories-container">
+        <div className="categories-btns-container">
+          <button className="categories-btn">All movies</button>
+          <button className="categories-btn">Action</button>
+          <button className="categories-btn">Comedy</button>
+          <button className="categories-btn">Sci Fi</button>
+          <button className="categories-btn">Thriller</button>
+        </div>
+      </div>
     </>
   );
 };
