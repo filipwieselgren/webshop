c7e5cdf26208ed49c39c4125fa1de2c60a1eea39
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-11-09 23:17:04 +0100

    form

--- src/components/PaySection.tsx
+++ src/components/PaySection.tsx
@@ -9,49 +9,93 @@ const PaySection = () => {
     cvc: 0,
     name: "",
   });
+  const [cvcLength, setCvcLength] = useState("");
+  const [cardNumberLength, setCardNumber] = useState("");
+  const [monthYearLength, setMonthYear] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+
+    if (name === "cvc") {
+      const limit = 4;
+      setCvcLength(e.target.value.slice(0, limit - 1));
+    }
+    if (name === "cardNumber") {
+      const limit = 13;
+      setCardNumber(e.target.value.slice(0, limit - 1));
+    }
+    if (name === "monthYear") {
+      const limit = 5;
+      setMonthYear(e.target.value.slice(0, limit - 1));
+    }
+
+    console.log();
+
+    setDetails((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <div className="payment-wrapper">
         <h4 className="payment-title">
           Fill in your information below and you are ready to stream.
         </h4>
-        <form className="payform">
+        <form className="payform" onSubmit={handleSubmit}>
           <label htmlFor="form-email">Email</label>
           <input
             type="email"
+            name="email"
             id="form-email"
             className="emailInput payInput"
             required
+            onChange={handleChange}
           />
           <label htmlFor="card-details">Card Details</label>
           <div className="card-details-wrapper">
             <input
               type="number"
+              name="cardNumber"
+              value={cardNumberLength}
               className="payInput cardNumber"
               id="card-details"
               placeholder="123 123 123 123"
               required
+              onChange={handleChange}
             />
             <input
               type="number"
+              name="monthYear"
+              value={monthYearLength}
               className="payInput cardMY"
               placeholder="MM/YY"
               required
+              onChange={handleChange}
             />
             <input
               type="number"
+              name="cvc"
+              value={cvcLength}
               className="payInput cvcNumber"
               placeholder="CVC"
               required
+              onChange={handleChange}
             />
           </div>
 
           <label htmlFor="cardName">Name on card</label>
           <input
             type="text"
+            name="name"
             className="nameInput payInput"
             id="cardName"
             required
+            onChange={handleChange}
           />
 
           <button type="submit" className="payBtn">
